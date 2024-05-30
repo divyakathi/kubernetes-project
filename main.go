fd052fb3fe9ca1ececbba3fd894d96076d3cd3c7
Divya <kathidivyareddyls@gmail.com>
2024-05-30 15:44:20 -0600

    intial commit

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Learning Kubernetes")
+	//Follow below link to understand ingress with practical example
+	// https://www.youtube.com/watch?v=a4Kq6mX4Hr4
+}
